b91610553c80871f22c06eec590bc86976295bf3
Michael Shoemaker <mikeshoemaker2000@yahoo.com>
2025-01-18 14:51:51 -0600

    Final Version After Recording

--- insert_script.py
+++ insert_script.py
@@ -1,8 +1,15 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
+print('Creating Engine')
 engine = create_engine("postgresql+psycopg2://admin:admin@postgres:5432/demo_db")
 
-df = pd.read_csv('https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2020-05.csv.gz', low_memory=False)
+print('Reading CSV')
+df = pd.read_csv('https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/taxi_zone_lookup.csv', low_memory=False)
 
-df.to_sql(con=engine, name='yellow_demo', if_exists='replace')
+print('Inserting Records')
+
+try:
+    df.to_sql(con=engine, name='taxi_zones', if_exists='append')
+except Exception as e:
+    print(f"Oops encounterd an issue {str(e)}")
